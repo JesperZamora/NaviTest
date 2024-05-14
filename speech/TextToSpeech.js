c49da30406b5d217e5dc0747eb5225eedb399a00
JesperZamora <jesp78m6@stud.kea.dk>
2024-05-14 16:50:28 +0200

    made changes to the speech component, so you now can stop and start it. There is still some bug with the callback 'onDone'. But you can now start it and stop it.

--- speech/TextToSpeech.js
+++ speech/TextToSpeech.js
@@ -1,33 +1,36 @@
-import * as React from 'react';
-import { View, StyleSheet, Button, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect} from 'react';
+import {TouchableOpacity } from 'react-native';
 import * as Speech from 'expo-speech';
 import { FontAwesome } from '@expo/vector-icons';
 
 
 export default function TextToSpeech({ say }) {
-  const speak = () => {
-    const thingToSay = `${say.task}`;
-    const options = {
-      language: 'en-US', // Specify the language
-      pitch: 0.5,        // Adjust the pitch (default is 1.0)
-      rate: 0.75,        // Adjust the speed of the speech (default is 1.0)
-      volume: 0.8,       // Set the volume (80% of maximum volume)
-      voice: 'com.apple.ttsbundle.Samantha-compact'
-    };
-    Speech.speak(thingToSay, options);
+  const [isSpeaking, setIsSpeaking] = useState(false);
+
+  const toggleSpeech = () => {
+    if (isSpeaking) {
+      Speech.stop();
+    } else {
+      const thingToSay = `${say.task}`;
+      const options = {
+        language: 'en-US',
+        pitch: 0.5,
+        rate: 0.75,
+        volume: 0.8,
+        voice: 'com.apple.ttsbundle.Samantha-compact'
+      };
+      Speech.speak(thingToSay, options, {
+        onDone: () => setIsSpeaking(false),
+        onError: () => setIsSpeaking(false)
+      });
+    }
+    setIsSpeaking(!isSpeaking);
   };
+
+
   return (
-    <TouchableOpacity onPress={speak}>
-      <FontAwesome name="microphone" size={20} color="#929292" />
+    <TouchableOpacity onPress={toggleSpeech}>
+      <FontAwesome name="microphone" size={20} color={isSpeaking ? "#FF0000" : "#929292"} />
     </TouchableOpacity>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    padding: 8,
-  },
-});
